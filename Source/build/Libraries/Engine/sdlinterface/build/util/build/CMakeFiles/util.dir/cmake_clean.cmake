f4b8d517efadb16af35e90765b102746e3c3a76d
mdiluzio <thefoips@gmail.com>
2014-04-07 21:25:00 +0100

    removing build files

--- Source/build/Libraries/Engine/sdlinterface/build/util/build/CMakeFiles/util.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../../../../../../libutil.pdb"
-  "../../../../../../libutil.so"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/util.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
