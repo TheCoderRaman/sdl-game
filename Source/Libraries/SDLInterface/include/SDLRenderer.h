6276fa744f174cc93654fa2f82c43f9758e4fd02
mdiluzio <thefoips@gmail.com>
2014-04-10 23:47:54 +0100

    Removing SDL2 hack from the windows build. Still need a good workaround. Also adding in SDLRenderer class

---
+++ Source/Libraries/SDLInterface/include/SDLRenderer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+// Includes
+#include "types.h"
+
+// Forward declares
+class SDL_Renderer;
+class SDLWindow;
+
+// References for how to use SDL_Renderer can be found
+// here : https://wiki.libsdl.org/CategoryRender
+
+class SDLRenderer 
+{
+public:
+
+	SDLRenderer();
+	~SDLRenderer();
+
+	// Create
+	eError Create( SDLWindow* window );
+
+	// Render
+	eError Render();
+
+	// Destroy
+	eError Destroy();
+
+private:
+
+	// The actual SDL Renderer
+	SDL_Renderer* myRenderer;
+}
---
+++ Source/Libraries/SDLInterface/src/SDLRenderer.cpp
@@ -0,0 +1,41 @@
+#include "SDLRenderer.h"
+
+SDLRenderer::SDLRenderer()
+: myRenderer ( NULL )
+{
+
+}
+
+SDLRenderer::~SDLRenderer()
+{
+
+}
+
+// Create
+eError SDLRenderer::Create( SDLWindow* window )
+{
+	eError err = eError_noErr;
+
+	myRenderer = SDL_CreateRenderer( window, 
+			-1,  	// Uses whichever default device is available
+			0);		// Uses the default SDL_RENDERER_ACCELERATED
+
+	return err;
+}
+
+// Render
+eError SDLRenderer::Render()
+{
+	eError err = eError_noErr;
+	return err;
+}
+
+// Destroy
+eError SDLRenderer::Destroy()
+{
+	eError err = eError_noErr;
+
+	SDL_DestroyRenderer(myRenderer);
+
+	return err;
+}
