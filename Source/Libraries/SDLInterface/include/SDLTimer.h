f757ad536b336fda49fb893c9fd558d1909659d0
Marc Di Luzio <thefoips@gmail.com>
2014-04-14 23:02:00 +0100

    Class layout

--- Source/Libraries/SDLInterface/include/SDLTimer.h
+++ Source/Libraries/SDLInterface/include/SDLTimer.h
@@ -8,5 +8,35 @@
 #ifndef _SDLTIMER_H_
 #define _SDLTIMER_H_
 
+//! \brief all timer methods are in ms (milliseconds)
+typedef int ms;
+
+#define msToSec(ms) (ms/1000)
+
+//! \brief class to contain a timer, useful for timekeeping
+//! Could be extended with various stopwatch like abilities like laps and pause/play
+class SDLTimer
+{
+public:	
+	
+	//! \brief Get the number of ticks since program initialisation
+	static ms GetGlobalLifetime(void);
+
+	//! \brief Get number of ticks since
+	static ms GetGlobalTicksSince(ms since);
+
+	//! \brief Start the timer
+	void Start( void );
+
+	//! \brief Get the time passed since Start
+	ms GetTimePassed( void );
+
+	//! \brief Reset the timer
+	void Reset( void );
+
+private:
+
+	ms m_startTime;
+};
 
 #endif //_SDLTIMER_H_
