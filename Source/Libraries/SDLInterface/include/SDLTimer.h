2fb80effaf0f5764bdb83e6bbbb53c3ae4323227
Marc Di Luzio <thefoips@gmail.com>
2014-04-14 22:41:41 +0100

    Initial file checkin for SDLTimer

---
+++ Source/Libraries/SDLInterface/include/SDLTimer.h
@@ -0,0 +1,12 @@
+//! \file SDLTimer.h
+//!
+//! \author  Marc Di luzio
+//! \date    April 2014
+//!
+//! Header for SDLTimer.cpp
+//!
+#ifndef _SDLTIMER_H_
+#define _SDLTIMER_H_
+
+
+#endif //_SDLTIMER_H_
---
+++ Source/Libraries/SDLInterface/src/SDLTimer.cpp
@@ -0,0 +1,9 @@
+//! \file SDLTimer.cpp
+//!
+//! \author  Marc Di luzio
+//! \date    April 2014
+//!
+//! Container for various SDL_Timer functions as found here:
+//! https://wiki.libsdl.org/CategoryTimer?highlight=%28%5CbCategoryCategory%5Cb%29
+//!
+#include "SDLTimer.h"
