a55e450de41b93e52a694bec71005e92bf0babc5
Owain Davies <ploddish@gmail.com>
2014-05-27 19:46:26 +0100

    Adding in empty SDLAudio classes for the audio interface.

---
+++ Source/Libraries/SDLInterface/include/SDLAudio.h
@@ -0,0 +1,37 @@
+//! \file SDLAudio.h
+//!
+//! \author  Owain Davies
+//! \date    May 2014
+//!
+//! Header for SDLAudio.cpp
+//!	
+#ifndef _SDLAUDIO_H_
+#define _SDLAUDIO_H_
+
+#include "SDLCommon.h"
+
+// Forward declare SDL structs
+struct SDL_AudioSpec;
+
+// Start the SDLInterface Namespace
+namespace SDLInterface
+{
+
+	//! \brief an Audio class
+	class Audio
+	{
+	public:
+
+		// Constructor and destructor
+		Audio();
+		~Audio();
+
+
+	private:
+
+		//! \brief a test sound
+		SDL_AudioSpec* my_sdl_audiospec;
+	};
+}
+
+#endif //_SDLAUDIO_H_
---
+++ Source/Libraries/SDLInterface/src/SDLAudio.cpp
@@ -0,0 +1,19 @@
+//! \file SDLAudio.cpp
+//!
+//! \author  Owain Davies
+//! \date    May 2014
+//!
+//! This holds the interface between SDL Audio and LEngine audio systems
+//!	
+
+#include "SDLAudio.h"
+
+SDLInterface::Audio::Audio()
+{
+
+}
+
+SDLInterface::Audio::~Audio()
+{
+
+}
