be7994690c6d2d217bcf87ea91c0ca93d89c07ff
Marc Di Luzio <thefoips@gmail.com>
2014-06-15 15:52:33 +0100

    Base Pause System class design

---
+++ Source/Libraries/LEngine/include/LPauseSystem.h
@@ -0,0 +1,55 @@
+//! \file LPauseSystem.h
+//!
+//! \author  Marc Di luzio
+//! \date    Jun 2014
+//!
+//! Header for LPauseSystem.cpp
+//!
+#ifndef _LPAUSESYSTEM_H_
+#define _LPAUSESYSTEM_H_
+
+#include <atomic>
+
+//! Enum for pause flags
+// Not strongly typed as it's just a flag
+enum EPauseFlag
+{
+	Game		= 0x0001,
+	Render		= 0x0002,
+	Physics		= 0x0004,
+	Audio		= 0x0008,
+	Controls	= 0x0010,
+
+	Global		= 0xFFFF
+};
+
+class LPauseSystem
+{
+public:
+
+	//! \brief default constructor
+	LPauseSystem();
+
+	//! \brief default destructor
+	~LPauseSystem();
+
+	//! \brief set the current flags
+	void SetNextFlags(int flags);
+
+	//! \brief Get the current pause flags
+	int GetCurrentFlags() const; 
+
+	//! \brief flush the flags
+	void FlushNextFlags();
+
+private:
+
+	//! \brief Current pause flags
+	std::atomic<int>	m_currentFlags;
+
+	//! \brief Next pause flags
+	std::atomic<int>	m_nextFlags;
+
+};
+
+#endif //_LPAUSESYSTEM_H_
