78773e4b1a71991508fdf402b7206561fd732c38
Owain Davies <ploddish@gmail.com>
2014-06-23 20:53:39 +0100

    Teeny bit more work and exploration into what this'll need to do. Looks like it'll have to live maybe in the renderer? It passes back a Surface once you've asked it to draw some text. Will have to discuss.

--- Source/Libraries/LEngine/include/LText.h
+++ Source/Libraries/LEngine/include/LText.h
@@ -1,9 +1,9 @@
-//! \file LEngine.h
+//! \file LText.h
 //!
-//! \author  Marc Di luzio
-//! \date    April 2014
+//! \author  Owain Davies
+//! \date    June 2014
 //!
-//! Header for LEngine.cpp
+//! Header for LText.cpp
 //!
 #ifndef _LTEXT_H_
 #define _LTEXT_H_
@@ -17,28 +17,20 @@ class LText
 public:
 	LText( void );
 	~LText( void );
-	/*
-
-	void LoadMusic( const char* filename, const char* name );
-	void PlayMusic( const char* name, bool bShouldLoop );
-	void TogglePauseMusic( void );
-
-	void LoadSound( const char* filename, const char* name );
-	void PlaySound( const char* name );
-
-	void ChangeVolume( int newVolume );
+	
+	void LoadFont( const char* filename, const char* name, int size );
+	void DrawText( const char* name, int size, const char* text );
 
 private:
 
-	const int iMAX_MUSIC_TRACKS = 1;
-	const int iMAX_SOUNDS		= 1;
+	const int iMAX_FONTS= 1;
 
-	std::vector< std::tuple< SDLInterface::SDLMusicFile, const char* > > m_musicTracks;
-	std::vector< std::tuple< SDLInterface::SDLSoundFile, const char* > > m_sounds;
+	std::vector< std::tuple< SDLInterface::SDL_Font, const char*, int > > m_fonts;
+
+	SDLInterface::SDL_Font* FindFont( const char* name, int size );
 	
-	SDLInterface::Audio m_AudioClass;
+	SDLInterface::SDLText m_SDLTextLibrary;
 
-	*/
 };
 
 
--- Source/Libraries/LEngine/src/LText.cpp
+++ Source/Libraries/LEngine/src/LText.cpp
@@ -18,6 +18,28 @@ LText::LText()
 //====================================================
 LText::~LText()
 {
-	// Delete all music
+	// Delete all text
 	
+}
+
+//====================================================
+void LText::LoadFont( const char* filename, const char* name, int size )
+{
+	SDLInterface::SDL_Font font = m_SDLTextLibrary.OpenFont( filename, size );
+
+	m_fonts.push_back( std::make_tuple( font, name, size ) );
+}
+
+//====================================================
+void LText::DrawText( const char* name, int size, const char* text )
+{
+	// will need positioning etc
+}
+
+//====================================================
+SDLInterface::SDL_Font* LText::FindFont( const char* name, int size )
+{
+	SDLInterface::SDL_Font* toReturn = nullptr;
+
+	return toReturn;
 }
