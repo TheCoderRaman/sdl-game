6b61b620d153852cf46628fc7780c7fac870fcb9
Owain Davies <ploddish@gmail.com>
2014-07-24 16:24:31 +0100

    Adding an empty scoreGUI class that'll hold the strings for the scoring

---
+++ Source/GameOne/include/ScoreGUI.h
@@ -0,0 +1,25 @@
+//! \file  ScoreGUI.h
+//!
+//! \author  Owain Davies
+//! \date    July 2014
+//!
+//! This is the class that represents the paddle that will be used in the main Pong game
+//!
+
+#ifndef _SCORE_GUI_H_
+#define _SCORE_GUI_H_
+
+#include "LError.h"
+
+class ScoreGUI
+{
+public:
+
+	LError Create( void );
+	LError Destroy( void );
+
+private:
+
+};
+
+#endif
---
+++ Source/GameOne/src/ScoreGUI.cpp
@@ -0,0 +1,32 @@
+//! \file ScoreGUI.cpp
+//!
+//! \author  Owain Davies
+//! \date    July 2014
+//!
+//! Score GUI class
+//!
+#include "ScoreGUI.h"
+
+#include "LEngine.h"
+#include "LError.h"
+#include "debug.h"
+
+
+//====================================================
+LError ScoreGUI::Create( void )
+{
+	RUNTIME_LOG( "Creating ScoreGUI..." );
+
+	LError err = LError::NoErr;
+
+
+	return err;
+}
+
+
+//====================================================
+LError ScoreGUI::Destroy( void )
+{
+
+	return LError::NoErr;
+}
